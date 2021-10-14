4240f5af515518d8ef44ad7805a6dee92ab32e2a
RyanChris7 <ryanchrist98@gmail.com>
2021-10-14 21:20:16 +0800

    navbar

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".mobile-nav-links");
+const links = document.querySelectorAll(".mobile-nav-links li");
+
 let menuOpen= false;
+
 menuBtn.addEventListener('click', () => {
     if(!menuOpen){
         menuBtn.classList.add('open');
@@ -8,5 +12,9 @@ menuBtn.addEventListener('click', () => {
         menuBtn.classList.remove('open');
         menuOpen= false
     }
-});
 
+    navLinks.classList.toggle("open");
+    // links.forEach(link => {
+    //   link.classList.toggle("fade");
+    // });
+});  
